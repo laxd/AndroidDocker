56c55b5630404c29ba830371b917945742fa019c
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2017-02-10 21:44:55 +0000

    Added exlplicit timeouts when connecting to docker service

--- app/src/main/java/uk/laxd/androiddocker/DockerServiceFactory.java
+++ app/src/main/java/uk/laxd/androiddocker/DockerServiceFactory.java
@@ -1,5 +1,10 @@
 package uk.laxd.androiddocker;
 
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -8,6 +13,15 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  * Created by lawrence on 11/01/17.
  */
 public class DockerServiceFactory {
+
+    private static final int TIMEOUT_SECONDS = 10;
+
+    // No need to check the HttpClient
+    // every time a new address is given.
+    private OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build();
 
     private DockerService instance;
 
@@ -20,11 +34,14 @@ public class DockerServiceFactory {
                 .baseUrl(address)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create())
+                .client(client)
                 .build()
                 .create(DockerService.class);
     }
 
     public void updateDockerAddress(String address) {
+        Log.i("DockerServiceFactory", "Updating docker address to '" + address + "'");
+
         instance = createWithAddress(address);
     }
 
