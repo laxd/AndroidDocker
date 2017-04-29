74de9646ba43e5b820b3758f92de76b41eef1200
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2017-04-29 20:49:23 +0100

    Added size to docker image

--- app/src/main/java/uk/laxd/androiddocker/dto/DockerImage.java
+++ app/src/main/java/uk/laxd/androiddocker/dto/DockerImage.java
@@ -2,6 +2,9 @@ package uk.laxd.androiddocker.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * Created by lawrence on 04/01/17.
  */
@@ -10,11 +13,22 @@ public class DockerImage extends DockerDto {
     @JsonProperty("RepoTags")
     private String[] repoTags;
 
+    @JsonProperty("VirtualSize")
+    private BigInteger size;
+
     public String[] getRepoTags() {
         return repoTags;
     }
 
     public void setRepoTags(String[] repoTags) {
         this.repoTags = repoTags;
+    }
+
+    public BigDecimal getSize() {
+        return size == null ? new BigDecimal("0") : new BigDecimal(size);
+    }
+
+    public void setSize(BigInteger size) {
+        this.size = size;
     }
 }
